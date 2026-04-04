685fda13cccafffd37e08054a1538089f7cfdd43
George Reda <dr.george.r.n@gmail.com>
2026-04-04 14:08:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    sw.js - Service Worker
    ======================================== */
 
-const CACHE_NAME = 'semester-4-cache-20260402-0953';
+const CACHE_NAME = 'semester-4-cache-20260404-0953';
 const urlsToCache = [
    './',
    './index.html',
