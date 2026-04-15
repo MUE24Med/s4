72763e6d29759092c175eb685ddb0085749abc88
George Reda <dr.george.r.n@gmail.com>
2026-04-15 22:22:37 +0200

    Update sw-register.js

--- sw-register.js
+++ sw-register.js
@@ -5,7 +5,7 @@
    ============================================================ */
 (function () {
   // ✅ المصدر الوحيد للحقيقة — غيّر هذا السطر فقط عند كل تحديث
-  const SW_CACHE_NAME = 'semester-4-cache-2026-04-14-0326';
+  const SW_CACHE_NAME = 'semester-4-cache-2026-04-15-1022';
 
   // احفظه في localStorage عشان config.js يقدر يقرأه
   localStorage.setItem('sw_cache_name', SW_CACHE_NAME);
